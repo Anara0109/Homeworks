c33d0a5ada2cb2c738311f3c788c640fb848a228
Малыбай Анар Ерболаткызы <MalybayA@lcoast.itstep>
2022-09-19 20:11:17 +0600

    HW

---
+++ Homework 29.08.2022/JS script.js
@@ -0,0 +1,85 @@
+// let num = 0
+
+// while( num <=10){
+//     console.log(num)
+
+//     num += 1;
+// }
+
+// for (let i = 0; i < 10; i += 1){   
+//     if (i % 2 !== 0){
+//         continue;
+//     }
+//     console.log(i);
+    
+//     // break continue
+// }
+
+
+// ---1 задача---1. Вывести # столько раз, сколько указал пользователь.
+
+// let loops = propmt()
+// let iter = 1
+
+// while(iter <= +loops){
+//     console.log('#');
+
+//     iter = iter + 1;
+// }
+
+
+// ---2 задача---2. Пользователь ввел число, а на экран вывелись все числа от введенного до 0.
+
+// let num = prompt
+
+// while (num != 0){
+//     console.log(num);
+
+//     num--;
+// }
+
+
+// // --3 задача--3. Запросить число и степень. Возвести число в указанную степень и вывести результат.
+
+// let base = 4
+// let degree = 3
+// let res = 1
+// while (degree > 0){
+//     res = res * base
+//     degree = degree - 1
+// }
+// console.log(res);
+
+
+// --4 задача--4. Запросить 2 числа и найти все общие делители.
+
+// let numb_1 = 76;
+// let numb_2 = 124;
+
+// let iter = 1;
+// let maxIter = numb_1 > numb_2 ? numb_1 : numb_2;
+
+// while (maxIter >= iter) {
+//     if (numb_1 % iter === 0 && numb_2 % iter === 0) {
+//         console.log(iter);
+//     }
+
+//     iter++;
+// }
+
+
+
+// n! = 1 * 2 * ... * n
+
+
+// // 5. Посчитать факториал введенного пользователем числа.
+
+// let num = 5;
+// let res = 1;
+
+// while (num >=1){
+//     res *= num;
+//     num--;
+// }
+
+// console.log(res);
